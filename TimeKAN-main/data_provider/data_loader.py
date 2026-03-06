623a26844a193953baae1393251ec7b07d2eaba4
yip0121 <156820645+yip0121@users.noreply.github.com>
2026-03-06 16:05:31 +0800

    add BatterySOH dataset loader for xlsx SOH sequence

--- TimeKAN-main/data_provider/data_loader.py
+++ TimeKAN-main/data_provider/data_loader.py
@@ -288,3 +288,67 @@ class Dataset_Custom(Dataset):
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
 
+
+class Dataset_BatterySOH(Dataset):
+    def __init__(self, root_path, flag='train', size=None,
+                 features='S', data_path='battery.xlsx',
+                 target='soh', scale=False, timeenc=0, freq='h', seasonal_patterns=None):
+        if size is None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        df_raw = pd.read_excel(os.path.join(self.root_path, self.data_path))
+
+        df_cycle_capacity = df_raw.iloc[:, :2].copy()
+        df_cycle_capacity.columns = ['cycle', 'capacity']
+        df_cycle_capacity['soh'] = df_cycle_capacity['capacity'] / df_cycle_capacity['capacity'].iloc[0]
+
+        data = df_cycle_capacity[['soh']].values
+
+        num_train = int(len(data) * 0.7)
+        num_test = int(len(data) * 0.2)
+        num_vali = len(data) - num_train - num_test
+
+        border1s = [0, num_train - self.seq_len, len(data) - num_test - self.seq_len]
+        border2s = [num_train, num_train + num_vali, len(data)]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = np.zeros((self.seq_len, 1), dtype=np.float32)
+        seq_y_mark = np.zeros((self.label_len + self.pred_len, 1), dtype=np.float32)
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
